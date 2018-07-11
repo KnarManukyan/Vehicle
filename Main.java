b7ade58dfc1eaabfb923e2f8a1103cb5200d29c6
KnarManukyan <Knarik_Manukyan@edu.aua.am>
2018-07-11 23:43:53 +0400

    first commit

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ class Vehicle {
     }
     public double calculateWeight(){ return weight;}
     public double calculateLength(){ return length;}
-    public void setWeight(double weight){ if(weight>0){this.weight = weight;}}
-    public void setLength(double length){ if(length>0){this.length = length;}}
+    public void setWeight(double weight){ if(weight>0){ this.weight = weight;}}
+    public void setLength(double length){ if(length>0){ this.length = length;}}
 }
 class Car extends Vehicle {
     Car(double weight, double length){
@@ -21,45 +21,47 @@ class Boat extends Vehicle {
     }
 }
 class Truck extends Vehicle {
-    private Trailor trailor = new Trailor();
+    private Trailer trailer = new Trailer();
     Truck(double weight, double length){
         super(weight,length);
     }
-    public void setTrailorWeight(double weight) { if(weight>0){ trailor.setweight(weight);}}
-    public void setTrailorLength(double length) { if(length>0){ trailor.setlength(length);}}
-    public void attachTrailor(){
-        setWeight(calculateWeight()+trailor.getWeight());
-        setLength(calculateLength()+trailor.getLength());
+    public void setTrailerWeight(double weight) { if(weight>0){ trailer.setweight(weight);}}
+    public void setTrailerLength(double length) { if(length>0){ trailer.setlength(length);}}
+    public void attachTrailer(){
+        setWeight(calculateWeight()+trailer.getWeight());
+        setLength(calculateLength()+trailer.getLength());
     }
-    public void detachTrailor(){
-        setWeight(calculateWeight()-trailor.getWeight());
-        setLength(calculateLength()-trailor.getLength());
+    public void detachTrailer(){
+        setWeight(calculateWeight()-trailer.getWeight());
+        setLength(calculateLength()-trailer.getLength());
     }
 
 }
-class Trailor{
+class Trailer{
     private double weight = 20;
     private double length = 20;
     public double getWeight() { return weight;}
     public double getLength() { return length;}
-    public void setweight(double weight) {this.weight = weight;}
-    public void setlength(double length) {this.length = length;}
+    public void setweight(double weight) { this.weight = weight;}
+    public void setlength(double length) { this.length = length;}
 }
 public class Main {
     public static void main(String [] args){
         double weight = 12;
         double length = 25;
+        double trailerlength = 30;
+        double trailerweight = 30;
         Vehicle car = new Car(weight,length);
         System.out.println("the length of the car is " + car.calculateLength());
         System.out.println("the weight of the car is " + car.calculateWeight());
         Truck tr = new Truck(weight,length);
-        tr.setTrailorLength(30);
-        tr.setTrailorWeight(30);
-        tr.attachTrailor();
-        System.out.println("The length after attaching the tailor " + tr.calculateLength());
-        System.out.println("The weight after attaching the tailor " + tr.calculateWeight());
-        tr.detachTrailor();
-        System.out.println("The length after detaching the tailor " + tr.calculateLength());
-        System.out.println("The weight after detaching the tailor " + tr.calculateWeight());
+        tr.setTrailerLength(trailerlength);
+        tr.setTrailerWeight(trailerweight);
+        tr.attachTrailer();
+        System.out.println("The length after attaching the trailer " + tr.calculateLength());
+        System.out.println("The weight after attaching the trailer " + tr.calculateWeight());
+        tr.detachTrailer();
+        System.out.println("The length after detaching the trailer " + tr.calculateLength());
+        System.out.println("The weight after detaching the trailer " + tr.calculateWeight());
     }
 }
